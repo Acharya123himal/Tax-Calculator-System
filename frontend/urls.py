05a6d73683f192dce08a053099c3e64a9b7a78f4
Himal Acharya <himal.acharya12377@gmail.com>
2022-05-08 20:05:42 +0545

    requirements.txt updated

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import ProfileView
 
 urlpatterns = [
     path('',views.getstarted,name="get-started"),
     path('contact/',views.feedback,name="contact"),
     path('about/',views.about,name="about"),
     path('instructions/',views.instructions,name="instructions"),
-    path('profile/',views.profile,name="instructions"),
+    path('profile/',ProfileView.as_view(),name="Profile"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.views.generic import ListView
+from authentication.models import User
 user = get_user_model()
 
 def getstarted(request):
@@ -18,20 +20,34 @@ def about(request):
 def instructions(request):
     return render(request, 'howto.html')
 
-@login_required(login_url = "login")
-def profile(request):
-    if request.method=="POST":
-        user.address="Ghodaghodi"
-        # user.first_name
-        # user.last_name
-        # user.username
-        # user.gender
-        # user.email
-        # user.state
-        # user.tel
+# @login_required(login_url = "login")
+# def profile(request):
+#     if request.method=="POST":
+#         user.save()
+#         messages.success(request,'Update Successful')
+#     return render(request, 'profile.html')
+
+
+class ProfileView(ListView):
+    model=User
+    def post(self,request):
+        user.address=request.POST.get('address')
+        user.first_name=request.POST.get('fname')
+        user.last_name=request.POST.get('lname')
+        user.username=request.POST.get('username')
+        user.gender=request.POST.get('gender')
+        user.email=request.POST.get('email')
+        user.state=request.POST.get('address')
+        user.tel=request.POST.get('tel')
+        user.marital=request.POST.get('marital_status')
+        user.resident=request.POST.get('resident')
         user.save()
         messages.success(request,'Update Successful')
-    return render(request, 'profile.html')
+        return redirect('profile')
+    def get(self,request):
+        return render(request, 'profile.html')
+
+
 
 def page_not_found_view(request,exception):
     return render(request, '404page.html')
