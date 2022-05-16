3c2403acbd3b7d88e6ccbb074a85c9f662b84f9c
Himal Acharya <himal.acharya12377@gmail.com>
2022-05-16 15:50:43 +0545

    final version

--- tax_management_system/views.py
+++ tax_management_system/views.py
@@ -3,4 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 
 def admin_check(request):
-    return HttpResponseRedirect("login/")
+    if user.is_authenticated:
+        return HttpResponseRedirect("dashboard/")
+    else:
+        return HttpResponseRedirect("login/")
